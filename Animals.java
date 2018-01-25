0a77698115f13a97e9ce9200a64e33d7ed6e25a8
Adityagkar <ghodgaonkaraditya@gmail.com>
2018-01-25 23:05:25 +0530

    Abstract class (Animal) example
    Abstract class (Animal) example

--- Animals.java
+++ Animals.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AbstractClass;
 
 
@@ -16,5 +12,5 @@ abstract void eat();
 void speak(){
     System.out.println("Growl !");
 }
-    
+  
 }
