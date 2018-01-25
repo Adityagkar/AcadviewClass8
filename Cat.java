9a2b8c5a99a7219f53122e20acb992615aa100f4
ghodgaonkaraditya <ghodgaonkaraditya@gmail.com>
2018-01-25 20:09:42 +0530

    Animal Interface

---
+++ Cat.java
@@ -0,0 +1,18 @@
+package InterfaceAnimal;
+
+
+public class Cat implements Animal {
+
+      @Override
+    public void speak() {
+        System.out.println("Meow !");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Cat Food !");
+  
+}
+    
+    
+}
---
+++ Dog.java
@@ -0,0 +1,17 @@
+package InterfaceAnimal;
+
+
+public class Dog implements Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("Bark !");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Pedigree !");
+    }
+    
+    
+}
