3e988778242619e0b7035afa48a00f41a2bf1d18
Adityagkar <ghodgaonkaraditya@gmail.com>
2018-01-25 20:15:54 +0530

    Class 8 assignment
    Take two numbers (string) and print larger of the two.

---
+++ String2IntMax.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class String2IntMax{
+  public static void main(String args[]){
+
+  System.out.println("Enter the first number");
+  Scanner scan = new Scanner(System.in);
+  String str1=scan.next();
+
+
+  System.out.println("Enter the second number");
+  String str2=scan.next();
+
+  if(Integer.parseInt(str1)>Integer.parseInt(str2))
+       System.out.println(""+str1+" is the largest number");
+  else
+      System.out.println(""+str2+" is the largest number");
+  
+
+  }
+}
